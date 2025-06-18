67aa415fe85ec16949898e7a19adfcb2e48851b2
Jessica Santos <jessiicansantos@gmail.com>
2025-06-18 15:37:55 -0300

    Fix: bg cutted in Hero component

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,37 +6,39 @@ const Hero = () => {
   return (
     <>
       {hero?.map((h: any, i: number) => (
-        <div key={`h-${i}`} className="hero">
-          <div key={h.id} className="left">
-            <h6>
-              {h.subTitle}
-            </h6>
-            <h1>
-              {h.title}
-            </h1>
-            <Link
-              to={`category/${h.category}`}
-              className="shopnow"
-            >
-              {h.hrefText}
-              <ArrowLongRight />
-            </Link>
-            <div className="customersCircle">
-              <div className="persons">
-                {h.customers?.map((c: any) => (
-                  <img key={c.id} src={c.src} alt={c.alt} />
-                ))}
-                <p>+</p>
-                <div className="txt">
-                  <p>{h.text}</p>
+        <div className="hero-bg">
+          <div key={`h-${i}`} className="hero">
+            <div key={h.id} className="left">
+              <h6>
+                {h.subTitle}
+              </h6>
+              <h1>
+                {h.title}
+              </h1>
+              <Link
+                to={`category/${h.category}`}
+                className="shopnow"
+              >
+                {h.hrefText}
+                <ArrowLongRight />
+              </Link>
+              <div className="customersCircle">
+                <div className="persons">
+                  {h.customers?.map((c: any) => (
+                    <img key={c.id} src={c.src} alt={c.alt} />
+                  ))}
+                  <p>+</p>
+                  <div className="txt">
+                    <p>{h.text}</p>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-          <div className="right">
-            {h.banner?.map((b: any) => (
-              <img key={b.id} src={b.src} alt={b.alt} className={b.id === 0 ? "imgTop" : b.id === 1 ? "imgBottom" : b.id === 2 ? "imgCenter" : ""} />
-            ))}
+            <div className="right">
+              {h.banner?.map((b: any) => (
+                <img key={b.id} src={b.src} alt={b.alt} className={b.id === 0 ? "imgTop" : b.id === 1 ? "imgBottom" : b.id === 2 ? "imgCenter" : ""} />
+              ))}
+            </div>
           </div>
         </div>
       ))}
