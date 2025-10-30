7bed627fc500b61b2db16d1d61533af8d6b99ab4
Jessica Santos <jessiicansantos@gmail.com>
2025-10-30 14:38:56 -0300

    Add ContactUpload to components

---
+++ src/components/ContactUpload/ContactUpload.tsx
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import ArrowUpTrayIcon from "../../assets/img/arrow-up-tray.svg";
+import "../../index.css";
+import "../../pages/Contact/Contact.css";
+import { ContactUploadType } from "../../pages/Contact/Contact";
+
+const ContactUpload = ({ register, errors }: ContactUploadType) => {
+  const [ selectedFile, setSelectedFile ] = useState('');
+
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+
+    if(file) {
+      setSelectedFile(file.name);
+    } else {
+      setSelectedFile("");
+    }
+  }
+
+  return (
+    <div className="upload">
+      <label htmlFor="cover-photo">
+        Upload file
+      </label>
+      <div className="box">
+        <div>
+          {<img src={ArrowUpTrayIcon} alt="Arrow Up Icon" width={30} height={30}  /> }
+          <div className="file-upload-wrapper">
+            <label
+              htmlFor="file-upload"
+            >
+              <span>Upload a file</span>
+              <input 
+                id="file-upload" 
+                type="file"
+                {...register("upload")}
+                onChange={handleFileChange}
+              />
+            </label>
+            <p>or drag and drop</p>
+          </div>
+          <p>PNG, JPG, GIF up to 10MB</p>
+        </div>
+        {selectedFile && (
+          <p className="uploaded-name">
+            Arquivo selecionado: <strong>{selectedFile}</strong>
+          </p>
+        )}
+      </div>
+      <p>{errors.upload?.message}</p>
+    </div>
+  );
+};
+
+export default ContactUpload;
